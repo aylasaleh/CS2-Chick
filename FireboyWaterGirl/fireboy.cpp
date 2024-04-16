b063db23ee7e0594b7a11b46e9129729e87eee00
aylasaleh <aylasaleh@aucegypt.edu>
2024-04-16 09:10:45 +0200

    Jump Right and Left fixed
    Still need to fix the fall after the jump with a qtimer

--- FireboyWaterGirl/fireboy.cpp
+++ FireboyWaterGirl/fireboy.cpp
@@ -8,11 +8,23 @@ FireBoy::FireBoy(QGraphicsItem* parent) : Players(parent) {
 }
 
 void FireBoy::keyPressEvent(QKeyEvent* event) {
-    if (event->key() == Qt::Key_Up) {
+    if ((event->key() == Qt::Key_Up)&&(event->key() == Qt::Key_Right)) {
         if (!isJumping) {
             isJumping = true;
             originalY = y();
-            jump(15);
+            jump(0);
+        }
+    }else if ((event->key() == Qt::Key_Up)&&(event->key() == Qt::Key_Left)) {
+        if (!isJumping) {
+            isJumping = true;
+            originalY = y();
+            jump(5);
+        }
+    }else if (event->key() == Qt::Key_Up) {
+        if (!isJumping) {
+            isJumping = true;
+            originalY = y();
+            jump(10);
         }
     } else if (event->key() == Qt::Key_Left) {
         moveBy(-10, 0);
@@ -22,12 +34,24 @@ void FireBoy::keyPressEvent(QKeyEvent* event) {
 }
 
 void FireBoy::jump(int jumpStep) {
-    if (jumpStep < 20) {
-        moveBy(0, -5);
+    if (jumpStep < 5) {    //if jumping right
+        moveBy(10, -10);
         ++jumpStep;
         QTimer::singleShot(20, this, [this, jumpStep]() { jump(jumpStep); });
-    } else {
+    }else if (jumpStep < 10) { //if jumping left
+        moveBy(-10, -10);
+        ++jumpStep;
+        QTimer::singleShot(20, this, [this, jumpStep]() { jump(jumpStep); });
+    }else if (jumpStep < 15) { //if jumping only
+        moveBy(0, -10);
+        ++jumpStep;
+        QTimer::singleShot(20, this, [this, jumpStep]() { jump(jumpStep); });
+    }else {                         //why do we need this else?
         isJumping = false;
+        while (y() != originalY)
+        {
+            QTimer::singleShot(20, this, [this]() { moveBy(0, 10); });
+        }
         setPos(x(), originalY);
     }
 }
